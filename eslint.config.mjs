6ac39c6428783b669458dc431257624dc75f8959
Anders Ah <37335037+AndersAhren@users.noreply.github.com>
2026-06-03 19:12:50 +0200

    part 3

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from '@eslint/js'
+import globals from 'globals'
+
+export default [
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**'
+    ],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
+]
